b665f76358e2c5e15f994ea189e30f307838f7cd
sericelena <sericelena@gmail.com>
2023-12-22 10:04:56 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL='https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC=10;
+export const RES_PER_PAGE=10;
+export const KEY='da410797-af8c-466a-b47b-362dc1857db9';
+export const MODAL_CLOSE_SEC=2.5;
